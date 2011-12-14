2019c77a862d8173119bfaea38919205c97a8a0f
Dmitry Mozzherin <dmozzherin@gmail.com>
2011-12-14 13:42:01 -0500

    harvesting names with authorities from wikispecies

--- bin/dwca-hunt.rb
+++ bin/dwca-hunt.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'dwca-hunter'
-# opts = { download: false, unpack: false }
-opts = {}
+opts = { download: false, unpack: false }
 DwcaHunter::logger = Logger.new($stdout)
 r = DwcaHunter::ResourceWikispecies.new(opts)
 dh = DwcaHunter.new(r)
--- lib/dwca-hunter/resource_wikispecies.rb
+++ lib/dwca-hunter/resource_wikispecies.rb
@@ -2,6 +2,7 @@
 class DwcaHunter
   class ResourceWikispecies < DwcaHunter::Resource
     def initialize(opts = {})
+      @tmp = open('tmp.csv', 'w:utf-8')
       @title = "Wikispecies"
       @url = "http://dumps.wikimedia.org/specieswiki/latest/specieswiki-latest-pages-articles.xml.bz2"
       @url = opts[:url] if opts[:url]
@@ -16,6 +17,7 @@ class DwcaHunter
 
     def make_dwca
       enrich_data
+      process_data
       generate_dwca
     end
 
@@ -38,6 +40,7 @@ class DwcaHunter
       id = nil
       count = 0
       @data = []
+      data_blob = nil
       f.each do |l|
         if !get_page_info && l.match(page_start)
           get_page_info = true
@@ -49,23 +52,89 @@ class DwcaHunter
             get_page_info = false
           end
         end
-        if !species_on && title && !title.match(/Wikispecies/i) && (l.match(name_re) || l.match(classification_re))
+        if !species_on && title && !title.match(/Wikispecies/i) && !title.match(/Template:/i) && (l.match(name_re) || l.match(classification_re))
           species_on = true
-          @data << { scientificName: DwcaHunter::XML.unescape(title).to_json, taxonId: id }
+          @data << { canonicalForm: DwcaHunter::XML.unescape(title), taxonId: id }
+          data_blob = [l]
           count += 1
           DwcaHunter::logger_write(self.object_id, "Extracted %s species data" % count) if count % BATCH_SIZE == 0
+        elsif species_on
+          data_blob << l
+          if l.match(page_end)
+            species_on = false 
+            @data[-1][:blob] = data_blob
+            data_blob = nil
+          end
         end
-        species_on = false if l.match(page_end)
       end
       DwcaHunter::logger_write(self.object_id, "Extracted total %s species data" % count)
       f.close
     end
 
+    def process_data
+      @data.each do |taxa|
+        find_name(taxa)
+      end
+    end
+
+    def find_name(taxa)
+      name_re = /^\s*\=\=\s*Name\s*\=\=\s*$/i
+      name_index = nil
+      taxa[:scientificName] = taxa[:canonicalForm]
+      taxa[:blob].each_with_index do |l, i|
+        if l.match(name_re)
+          name_index = i + 1
+          break
+        end
+        name_index = -1
+      end
+      return if name_index == -1
+      while taxa[:blob][name_index].strip.empty?
+        name_index += 1
+      end
+      if name = parse_name(taxa[:blob][name_index].strip, taxa)
+        taxa[:scientificName] = name
+        tmp = "%s\t%s\n" % [taxa[:canonicalForm], taxa[:scientificName]]
+        @tmp.write(tmp)
+      end
+    end
+
+    def parse_name(name_string, taxa)
+      name_string.gsub!(/\<\/text\>/, '')
+      name_string.gsub!(/\<\/comment\>/, '')
+      name_string.gsub!('BASEPAGENAME', taxa[:canonicalForm])
+      if name_string.match(/\</) || name_string.match(/\=\=/)
+        puts "%s:::%s" % [taxa[:canonicalForm], name_string]
+        return
+      end
+      name_string = name_string.strip
+      old_l = name_string.dup
+      name_string.gsub! /^\*\s*/, ''
+      name_string.gsub!(/\[\[([^\]]+\|)?([^\]]*)\]\]/, '\2')
+      name_string.gsub!(/\{\{([^\}]+\|)?([^\}]*)\}\}/, '\2')
+      name_string.gsub!(/[']{2,}/, ' ')
+      name_string.gsub!(/["]{2,}/, ' ')
+      name_string.gsub!(/\:\s*\d.*$/, '')
+      name_string.gsub!(/,\s*\[RSD\]/i, '')
+      name_string.gsub!(/^\s*†\s*/, '')
+      name_string.gsub!(/(:\s*)?\[http:[^\]]+\]/, '')
+      name_string = DwcaHunter::XML.unescape(name_string)
+      name_string.gsub!(/\<nowiki\>.*$/, '')
+      name_string.gsub!(/\<br\s*[\/]?\s*\>/, '')
+      name_string.gsub!(/^\s*\&dagger;\s*/, '')
+      name_string.gsub!(/&nbsp;/, ' ')
+      name_string.gsub!(/\s+/, ' ')
+      name_string = name_string.strip
+      # puts "%s---%s" % [name_string, old_l]
+      return name_string
+    end
+
+
     def generate_dwca
       DwcaHunter::logger_write(self.object_id, "Creating DarwinCore Archive file")
       gen = DarwinCore::Generator.new(File.join(@download_dir, "dwca.tar.gz"))
-      core = [["http://rs.tdwg.org/dwc/terms/taxonID", "http://rs.tdwg.org/dwc/terms/scientificName"]]
-      core += @data.map { |d| [d[:taxonId], d[:scientificName]] }
+      core = [["http://rs.tdwg.org/dwc/terms/taxonID", "http://rs.tdwg.org/dwc/terms/scientificName", "http://globalnames.org/terms/canonicalForm"]]
+      core += @data.map { |d| [d[:taxonId], d[:scientificName], d[:canonicalForm]] }
       eml = {
         :id => @uuid,
         :title => @title,
