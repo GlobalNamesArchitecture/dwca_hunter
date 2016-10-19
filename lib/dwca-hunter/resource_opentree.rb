2cf7a6d02a84322176be9ed46fdf8cea4474f2cd
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-10-18 21:17:09 -0500

    wip

--- lib/dwca-hunter/resource_opentree.rb
+++ lib/dwca-hunter/resource_opentree.rb
@@ -10,6 +10,29 @@ class DwcaHunter
       @extensions = []
       @count = 1
       @clades = {}
+      @core = [["http://rs.tdwg.org/dwc/terms/taxonId",
+                "http://globalnames.org/terms/localID",
+                "http://purl.org/dc/terms/scientificName",
+                "http://purl.org/dc/terms/parentNameUsageId",
+                "http://purl.org/dc/terms/taxonRank",
+                "http://globalnames.org/ottCrossMaps",
+                "http://globalnames.org/ottNotes"]]
+      @eml = {
+        id: @uuid,
+        title: @title,
+        authors: [{ url: "https://tree.opentreeoflife.org" }],
+        abstract: "Open Tree of Life aims to construct a comprehensive, " \
+                  "dynamic and digitally-available tree of life by " \
+                  "synthesizing published phylogenetic trees along with" \
+                  "taxonomic data. The project is a collaborative effort" \
+                  "between 11 PIs across 10 institutions.",
+        metadata_providers: [
+          { first_name: "Dmitry",
+            last_name: "Mozzherin",
+            email: "dmozzherin@gmail.com" }
+        ],
+        url: @url
+      }
       @url = "http://opendata.globalnames.org/id-crossmap/ott2.10draft11.tgz"
       @download_path = File.join(DEFAULT_TMP_DIR, "dwca_hunter",
                                  "opentree", "data.tar.gz")
@@ -47,17 +70,51 @@ class DwcaHunter
     end
 
     def classification
+      @classification = []
+      @names = {}
       DwcaHunter.logger_write(object_id, "Building classification")
       open(@taxonomy).each_with_index do |line, i|
         if ((i + 1) % BATCH_SIZE).zero?
           DwcaHunter.logger_write(object_id,
                                   "Traversed #{i + 1} taxonomy lines")
         end
-        puts line.split("|").map(&:strip).join("|")
+        @classification << line.split("|").map(&:strip)
       end
     end
 
     def generate_dwca
+      DwcaHunter.logger_write(object_id, "Creating DarwinCore Archive file")
+      DwcaHunter.logger_write(object_id, "Assembling Core Data")
+      generate_core
+      generate_synonyms
+      super
+    end
+
+    def generate_core
+      @classification.each_with_index do |d|
+        if (count % BATCH_SIZE).zero?
+          DwcaHunter.logger_write(object_id, "Traversing #{count} core " \
+                                  "data record")
+        end
+        @core << [d[0], d[0], d[2], d[1], d[3], d[4], d[5]]
+      end
+    end
+
+    def synonyms
+      []
+    end
+
+    def generate_synonyms
+      @extensions <<
+        { data: [["http://rs.tdwg.org/dwc/terms/taxonId",
+                  "http://rs.tdwg.org/dwc/terms/scientificName",
+                  "http://rs.tdwg.org/dwc/terms/taxonomicStatus"]],
+          file_name: "synonyms.txt" }
+
+      synonyms.each do |synonym|
+        @extensions.first[:data] << [d[:id], synonym[:scientificName],
+                                     synonym[:taxonomicStatus]]
+      end
     end
   end
 end
