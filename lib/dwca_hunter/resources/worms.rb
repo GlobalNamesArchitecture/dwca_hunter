f1a53e46e6a44f2b772ba5530f12de55c087ad7f
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-02-02 12:42:18 +0000

    add worms

--- lib/dwca_hunter/resources/worms.rb
+++ lib/dwca_hunter/resources/worms.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
+# rubocop:disable all
 module DwcaHunter
   class ResourceWoRMS < DwcaHunter::Resource
-    def initialize(opts = { download: false, unpack: false })
+    def initialize() # opts = { download: false, unpack: false })
       @command = "worms"
       @title = "World Register of Marine Species"
       # Download using sf33 machine
       # Put to box.com to get the same download link
       @url = "https://uofi.box.com/shared/static/s5smmpz9o36t08pimaw5exp74h5q8aag.zip"
-      @UUID = "a5299f97-a746-47a4-bf11-4090a45168ce"
+      @UUID = "bf077d91-673a-4be4-8af9-76db45d07e98"
       @download_path = File.join(Dir.tmpdir,
                                  "dwca_hunter",
                                  "worms",
@@ -17,7 +18,6 @@ module DwcaHunter
       @names = []
       @vernaculars = []
       @extensions = []
-      @synonyms_hash = {}
       @vernaculars_hash = {}
       super(opts)
     end
@@ -41,6 +41,20 @@ module DwcaHunter
     def get_names
       Dir.chdir(@download_dir)
       collect_names
+      collect_vernaculars
+    end
+    
+    def collect_vernaculars
+      file = CSV.open(File.join(@download_dir, "vernacularname.txt"),
+                      headers: true, col_sep: "\t", quote_char: "\b")
+      file.each_with_index do |row, i|
+        taxon_id = row["taxonID"].strip
+        vern_name = row["vernacularName"].strip
+        lang = row["language"].to_s.strip 
+        @vernaculars << { taxon_id: taxon_id, vern: vern_name, lang: lang }
+        puts "Processed %s vernaculars" % i if i % 10_000 == 0
+      end 
+      
     end
 
     def collect_names
@@ -54,7 +68,6 @@ module DwcaHunter
         rank = row["taxonRank"].to_s.strip
         accepted = row["acceptedNameUsageID"].to_s.strip
         accepted = taxon_id if accepted.empty?
-        year = row["Year_of_effective_publication"]
         status = row["taxonomicStatus"].to_s.strip
         code = row["nomenclaturalCode"].to_s.strip
         kingdom = row["kingdom"].to_s.strip
@@ -103,6 +116,23 @@ module DwcaHunter
                   n[:family], n[:genus], n[:status], n[:code]]
       end
 
+      @extensions << {
+        data: [[
+          "http://rs.tdwg.org/dwc/terms/taxonID",
+          "http://rs.tdwg.org/dwc/terms/vernacularName",
+          "http://rs.tdwg.org/dwc/terms/language"
+        ]],
+        file_name: "vernacular_names.txt",
+        row_type: "http://rs.gbif.org/terms/1.0/VernacularName"
+      }
+      
+      @vernaculars.each do |v|
+        @extensions[-1][:data] << [v[:taxon_id], v[:vern], v[:lang]]
+      end
+
+      @vernaculars.each do |v|
+        @extensions[-1][:data] << [v[:taxon_id], v[:vern]]
+      end
       @eml = {
         id: "http://dx.doi.org/10.14284/170",
         title: @title,
